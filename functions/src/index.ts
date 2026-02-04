16d19d37f67969535888249c2f86316efdafd7e3
seeyu44 <seeyu44@gmail.com>
2026-02-05 03:16:04 +0800

    function getCallHistory update

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -22,6 +22,7 @@ export {
   fixAdminUsername,
 } from "./auth";
 export {addReview, getReviews, getReviewStats, deleteReview, getFiveStarReviews} from "./reviews";
+export {getCallHistory, endCall} from "./calls";
 
 
 // Start writing functions
